83ba6f64b1cf08756c814786c20511c616e8f0c5
Alley Manalio <amanalio@ufl.edu>
2016-06-06 13:35:54 -0400

    Add five presidents info into an array

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 var iceCream = ['Banana', 'Cookie dough', 'Peanut butter',
                 'Sherbert'];
+
+var presidents = [
+    {firstName: 'Barack', lastName: 'Obama', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush, Jr.', termLength: '2 terms', party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush, Sr.', termLength: '1 term', party: 'Republican', yearsOfPresidency: 4},
+    {firstName: 'Ronald', lastName: 'Reagan', termLength: '2 terms', party: 'Republican', yearsOfPresidency: 8}
+];
