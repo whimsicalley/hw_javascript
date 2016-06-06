d971b59bb9a4aca67b9ca2abbaaca93cb9f57723
Alley Manalio <amanalio@ufl.edu>
2016-06-06 17:37:22 -0400

    Add numba function

--- js/main.js
+++ js/main.js
@@ -16,3 +16,17 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length === 7) {
+        console.log('7, what a perfect choice!');
+    } else {
+        console.log('I\'m sorry, but that\'s too many to count.');
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
