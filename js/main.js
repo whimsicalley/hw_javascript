04a2957a980e91420c50c0a4a84122b4f32e915d
Alley Manalio <amanalio@ufl.edu>
2016-06-10 13:33:46 -0400

    Correct yearsOfPresidency values
    Changed each of the yearsOfPresidency values from a number (ex: 8) to
    actual years the presidents were in office (ex: '2001-2009').

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ var iceCream = ['Banana', 'Cookie dough', 'Peanut butter',
                 'Sherbert'];
 
 var presidents = [
-    {firstName: 'Barack', lastName: 'Obama', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: 8},
-    {firstName: 'George', lastName: 'Bush, Jr.', termLength: '2 terms', party: 'Republican', yearsOfPresidency: 8},
-    {firstName: 'Bill', lastName: 'Clinton', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: 8},
-    {firstName: 'George', lastName: 'Bush, Sr.', termLength: '1 term', party: 'Republican', yearsOfPresidency: 4},
-    {firstName: 'Ronald', lastName: 'Reagan', termLength: '2 terms', party: 'Republican', yearsOfPresidency: 8}
+    {firstName: 'Barack', lastName: 'Obama', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: '2009 - present'},
+    {firstName: 'George', lastName: 'Bush, Jr.', termLength: '2 terms', party: 'Republican', yearsOfPresidency: '2001 - 2009'},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: '2 terms', party: 'Democrat', yearsOfPresidency: '1993 - 2001'},
+    {firstName: 'George', lastName: 'Bush, Sr.', termLength: '1 term', party: 'Republican', yearsOfPresidency: '1989 - 1993'},
+    {firstName: 'Ronald', lastName: 'Reagan', termLength: '2 terms', party: 'Republican', yearsOfPresidency: '1981 - 1989'}
 ];
 
 console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
